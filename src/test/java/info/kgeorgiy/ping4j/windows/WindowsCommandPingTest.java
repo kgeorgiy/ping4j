fe3ad443d2362d20c70d2137b874f6a5bc5b5744
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-06-26 12:25:49 +0300

    Gradle test configuration added

--- src/test/java/info/kgeorgiy/ping4j/windows/WindowsCommandPingTest.java
+++ src/test/java/info/kgeorgiy/ping4j/windows/WindowsCommandPingTest.java
@@ -1,12 +1,15 @@
 package info.kgeorgiy.ping4j.windows;
 
 import info.kgeorgiy.ping4j.AbstractPingTest;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  * Tests {@link WindowsCommandPing}.
  *
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
+@EnabledOnOs(OS.WINDOWS)
 class WindowsCommandPingTest extends AbstractPingTest {
     WindowsCommandPingTest() {
         super(new WindowsCommandPing(), !System.getProperty("os.name").startsWith("Windows"));
--- src/test/java/info/kgeorgiy/ping4j/windows/WindowsPingTest.java
+++ src/test/java/info/kgeorgiy/ping4j/windows/WindowsPingTest.java
@@ -1,12 +1,15 @@
 package info.kgeorgiy.ping4j.windows;
 
 import info.kgeorgiy.ping4j.AbstractPingTest;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  * Tests {@link WindowsPing}.
  *
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
+@EnabledOnOs(OS.WINDOWS)
 class WindowsPingTest extends AbstractPingTest {
     WindowsPingTest() {
         super(new WindowsPing(), !System.getProperty("os.name").startsWith("Windows"));
