499bbabbdb07bef30a7898b5a1dbdd8110ed494f
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-07-03 09:38:50 +0300

    Switched to context

--- ping4j-posix/src/main/c/ping4j-posix.c
+++ ping4j-posix/src/main/c/ping4j-posix.c
@@ -47,10 +47,36 @@ uint16_t checksum(void *buffer, size_t len) {
 void ping4jInit() {
 }
 
-bool setResult(struct Ping4jResult* pingResult, uint32_t result, uint32_t value) {
-    pingResult->result = result;
-    pingResult->value = value;
-    return false;
+static const int NO_SOCKET = -1;
+
+typedef struct Context {
+    int socket;
+    PING4J_RESULT* result;
+    uint16_t packetSize;
+} CONTEXT;
+
+void setResult(CONTEXT* context, uint32_t result, uint32_t value) {
+    context->result->result = result;
+    context->result->value = value;
+
+    if (context->socket != NO_SOCKET) {
+        close(context->socket);
+    }
+}
+
+void prologue(CONTEXT* context, uint16_t packetSize, PING4J_RESULT* result) {
+    context->socket = NO_SOCKET;
+    context->result = result;
+    context->packetSize = packetSize < MIN_PACKET_SIZE ? MIN_PACKET_SIZE : packetSize;
+}
+
+bool check(CONTEXT* context, int value) {
+    if (value == -1) {
+        setResult(context, RESULT_ERROR, errno);
+        return false;
+    } else {
+        return true;
+    }
 }
 
 static const int REPLY_SIZE = 65530;
@@ -60,22 +86,13 @@ struct Request {
     uint8_t reply[65530];
 };
 
-bool check(int value, struct Ping4jResult* result) {
-    if (value == -1) {
-        setResult(result, RESULT_ERROR, errno);
-        return false;
-    } else {
-        return true;
-    }
-}
-
 static const uint32_t ICMP_TIMED_OUT = 11010;
 static const uint32_t IP_HEADER_SIZE = 20;
 
 int64_t currentTimeMillis() {
     struct timespec spec;
     if (clock_gettime(CLOCK_MONOTONIC, &spec) == -1) {
-    	return -1;
+        return -1;
     }
     return spec.tv_sec * (int64_t) 1000 + spec.tv_nsec / 1000000;
 }
@@ -86,36 +103,26 @@ void ping4jPing4(
     struct Ping4jIpv4Address* address,
     uint32_t timeout,
     uint8_t ttl,
-    uint16_t packetSize,
-    struct Ping4jResult* result
+    uint16_t packetSize2,
+    struct Ping4jResult* result2
 ) {
-    if (packetSize < MIN_PACKET_SIZE) {
-        packetSize = MIN_PACKET_SIZE;
-    }
+    CONTEXT context;
+    prologue(&context, packetSize2, result2);
 
-    int sock = socket(AF_INET, SOCKET_TYPE, IPPROTO_ICMP);
-    if (!check(sock, result)) {
+    context.socket = socket(AF_INET, SOCKET_TYPE, IPPROTO_ICMP);
+    if (!check(&context, context.socket)) {
         return;
     }
-    printf("\t\tcreated\n");
+    printf("\t\tsocket\n");
 
     uint32_t ttl32 = ttl;
     if (
-        !check(setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &REPLY_SIZE, sizeof(REPLY_SIZE)), result) ||
-        !check(setsockopt(sock, IPPROTO_IP, IP_TTL, &ttl32, sizeof(ttl32)), result)
+        !check(&context, setsockopt(context.socket, SOL_SOCKET, SO_RCVBUF, &REPLY_SIZE, sizeof(REPLY_SIZE))) ||
+        !check(&context, setsockopt(context.socket, IPPROTO_IP, IP_TTL, &ttl32, sizeof(ttl32)))
     ) {
-        close(sock);
         return;
     }
     printf("\t\toptions\n");
-
-    uint32_t ttlRes;
-    uint32_t ttlSize = sizeof(ttlRes);
-    if (!check(getsockopt(sock, IPPROTO_IP, IP_TTL, &ttlRes, &ttlSize), result)) {
-        close(sock);
-        return;
-    }
-    printf("\t\tttl = %d\n", ttlRes);
 
     uint16_t id = getpid();
     uint16_t sequence = seq++;
@@ -127,32 +134,34 @@ void ping4jPing4(
     request.header.identifier = id;
     request.header.sequence = sequence;
 
-    for (unsigned int i = 0; i < packetSize - sizeof(request.header); i++) {
+    for (unsigned int i = 0; i < context.packetSize - sizeof(request.header); i++) {
         request.body[i] = (unsigned char) 'a' + i;
     }
-    request.header.checksum = checksum(&request, packetSize);
+    request.header.checksum = checksum(&request, context.packetSize);
 
     int64_t current = currentTimeMillis();
-    if (!check(current, result)) {
-        close(sock);
+    if (!check(&context, current)) {
         return;
     }
 
     const struct sockaddr_in toAddress = {.sin_family = AF_INET, .sin_addr = {*(uint32_t *) address->octets}};
-    if (!check(sendto(sock, &request, packetSize, 0, (struct sockaddr*) &toAddress, sizeof(toAddress)), result)) {
-        close(sock);
+    if (!check(&context, sendto(
+        context.socket,
+        &request, context.packetSize,
+        0,
+        (struct sockaddr*) &toAddress, sizeof(toAddress))
+    )) {
         return;
     }
-    printf("\t\tsend %x %d\n", toAddress.sin_addr.s_addr, packetSize);
+    printf("\t\tsendto %x %d\n", toAddress.sin_addr.s_addr, context.packetSize);
 
     int64_t deadline = current + timeout;
-
     while (true) {
-		int64_t remaining = deadline - current;
-    	struct timeval remainingVal = {.tv_sec = remaining / 1000, .tv_usec = remaining % 1000 * 1000};
-        if (!check(setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &remainingVal, sizeof(remainingVal)), result)) {
-        	close(sock);
-        	return;
+        int64_t remaining = deadline - current;
+
+        struct timeval remainingVal = {.tv_sec = remaining / 1000, .tv_usec = remaining % 1000 * 1000};
+        if (!check(&context, setsockopt(context.socket, SOL_SOCKET, SO_RCVTIMEO, &remainingVal, sizeof(remainingVal)))) {
+            return;
         }
 
         uint8_t reply[REPLY_SIZE];
@@ -160,31 +169,29 @@ void ping4jPing4(
         struct sockaddr_in fromAddress;
         socklen_t fromAddressLen = sizeof(fromAddress);
 
-        const ssize_t received = recvfrom(sock, &reply, sizeof(reply), 0, (struct sockaddr*) &fromAddress, &fromAddressLen);
+        const ssize_t received = recvfrom(context.socket, &reply, sizeof(reply), 0, (struct sockaddr*) &fromAddress, &fromAddressLen);
         printf("\t\treceived %ld\n", received);
+
         if (received == -1 && errno == EAGAIN) {
-            close(sock);
-            setResult(result, RESULT_STATUS, ICMP_TIMED_OUT);
+            setResult(&context, RESULT_STATUS, ICMP_TIMED_OUT);
             return;
         }
-        if (!check(received, result)) {
-            close(sock);
+        if (!check(&context, received)) {
             return;
         }
 
         current = currentTimeMillis();
-        if (!check(current, result)) {
-        	return;
+        if (!check(&context, current)) {
+            return;
         }
 
         if (current >= deadline) {
-        	close(sock);
-            setResult(result, RESULT_STATUS, ICMP_TIMED_OUT);
+            setResult(&context, RESULT_STATUS, ICMP_TIMED_OUT);
             return;
         }
 
         if (received < IP_HEADER_SIZE) {
-        	continue;
+            continue;
         }
 
         printf("\t\treceived: ");
@@ -197,7 +204,7 @@ void ping4jPing4(
         size_t headerLen = ((*(uint8_t *) reply) & 0xf) * 4;
         printf("\t\theaderLen: %ld\n", headerLen);
         if (received < headerLen + sizeof(PING4J_ICMP_ECHO)) {
-        	continue;
+            continue;
         }
 
         const PING4J_ICMP_ECHO* echo = (PING4J_ICMP_ECHO *) (reply + headerLen);
@@ -214,13 +221,14 @@ void ping4jPing4(
             echo->sequence != sequence ||
             fromAddress.sin_addr.s_addr != toAddress.sin_addr.s_addr
         ) {
-        	continue;
+            continue;
         }
 
-        if (!check(close(sock), result)) {
+        if (!check(&context, close(context.socket))) {
             return;
         }
-        setResult(result, RESULT_SUCCESS, 0);
+        context.socket = NO_SOCKET;
+        setResult(&context, RESULT_SUCCESS, 0);
         return;
     }
 }
@@ -232,5 +240,8 @@ void ping4jPing6(
     uint16_t packetSize,
     struct Ping4jResult* result
 ) {
-    setResult(result, RESULT_ERROR, 0xffff);
+    CONTEXT context;
+    prologue(&context, packetSize, result);
+
+    setResult(&context, RESULT_ERROR, 0xffff);
 }
