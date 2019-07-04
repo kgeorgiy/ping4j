666ddb586a5ff971dd73f0906b26eb0bf8dcbdb5
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-07-04 12:14:33 +0300

    ping-impl introduced

--- ping4j-windows/src/main/c/windows/ping-impl.c
+++ ping4j-windows/src/main/c/windows/ping-impl.c
@@ -153,19 +153,3 @@ void ping4jPing6(
     const PICMPV6_ECHO_REPLY reply = (PICMPV6_ECHO_REPLY) request.reply;
     finalize(&request, timeout, replies, reply->Status, reply->RoundTripTime, result);
 }
-
-void ping4jPing(
-    const PING4J_IP_ADDRESS* const address,
-    const uint32_t timeout,
-    const uint8_t ttl,
-    const uint16_t packetSize,
-    PING4J_RESULT* const result
-) {
-    if (address->version == PING4J_IPV4_VERSION) {
-        ping4jPing4(&address->u.ipv4, timeout, ttl, packetSize, result);
-    } else if (address->version == PING4J_IPV6_VERSION) {
-        ping4jPing6(&address->u.ipv6, timeout, ttl, packetSize, result);
-    } else {
-		assert(false);
-    }
-}
